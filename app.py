46d144f19c5e8d05dffdd0e5e017502bb799b381
bao.tran <bao.trancong.2003@gmail.com>
2024-06-09 13:27:00 +0700

    streamlit ui + v2 data prediction ipynb

--- app.py
+++ app.py
@@ -1,29 +1,112 @@
+import pandas as pd
+
 from flask import Flask, request, jsonify
+from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+
+def preprocess(data):
+    if (data['lat_cat'] == 'north'):
+        data['lat'] = 47.75
+    else:
+        data['lat'] = 47.25
+
+    if (data['long_cat'] == 'east'):
+        data['long'] = -122.25
+    else:
+        data['long'] = -122.75
+
+    if (data['sqft_living'] < 1500):
+        data['sqft_living_cat'] = 'small'
+    elif (data['sqft_living'] < 2500):
+        data['sqft_living_cat'] = 'medium'
+    elif (data['sqft_living'] < 3500):
+        data['sqft_living_cat'] = 'big'
+    else:
+        data['sqft_living_cat'] = 'very big'
+
+    if (data['sqft_lot'] < 5000):
+        data['sqft_lot_cat'] = 'small'
+    elif (data['sqft_lot'] < 10000):
+        data['sqft_lot_cat'] = 'medium'
+    elif (data['sqft_lot'] < 15000):
+        data['sqft_lot_cat'] = 'big'
+    else:
+        data['sqft_lot_cat'] = 'very big'
+
+    if (data['sqft_above'] < 1500):
+        data['sqft_above_cat'] = 'small'
+    elif (data['sqft_above'] < 2500):
+        data['sqft_above_cat'] = 'medium'
+    elif (data['sqft_above'] < 3500):
+        data['sqft_above_cat'] = 'big'
+    else:
+        data['sqft_above_cat'] = 'very big'
+
+    if (data['sqft_basement'] < 500):
+        data['sqft_basement_cat'] = 'small'
+    elif (data['sqft_basement'] < 1000):
+        data['sqft_basement_cat'] = 'medium'
+    elif (data['sqft_basement'] < 1500):
+        data['sqft_basement_cat'] = 'big'
+    else:
+        data['sqft_basement_cat'] = 'very big'
+
+    data['year'] = 2014
+    data['month'] = 5
+    data['sqft_living15'] = 2000
+    data['sqft_lot15'] = 5000
+    data['yr_built'] = 2000
+    data['yr_renovated'] = 0
+    data['built_cat'] = 'middle'
+    data['sqft_living15_cat'] = 'medium'
+    data['sqft_lot15_cat'] = 'medium'
+
+    return data
+
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    data = request.get_json(force=True)
+    data = {
+        'bedrooms': 4,
+        'bathrooms': 2,
+        'sqft_living': 20000,
+        'sqft_lot': 5000,
+        'floors': 1,
+        'waterfront': 0,
+        'view': 0,
+        'condition': 3,
+        'grade': 7,
+        'sqft_above': 1500,
+        'sqft_basement': 500,
+        'renovated_cat': 'not renovated',
+        'basement_cat': 'has basement',
+        'lat_cat': 'north',
+        'long_cat': 'east'
+    }
+    # data = request.get_json(force=True)
+    data = preprocess(data)
+    print(data)
+    data = pd.DataFrame([data])
+    return jsonify({'prediction': predict_price(data)})
 
-    # Extract parameters from the POST request body
-    param1 = data.get('param1')
-    param2 = data.get('param2')
-    # Add more parameters as needed
-
-    # Call your prediction function
-    prediction = predict_price(param1, param2)  # Replace with your function
-
-    # Return the prediction as JSON
-    return jsonify({'prediction': prediction})
 
 # write new rote index return hello world
 @app.route('/')
 def index():
     return "Hello World"
 
-def predict_price(param1, param2):
-    return param1 + param2
+
+def predict_price(data):
+    # Load the model
+    import joblib
+    model = joblib.load('model.pkl')
+
+    # Make prediction
+    prediction = model.predict(data)
+    return prediction[0]
+
 
 if __name__ == '__main__':
-    app.run(port=5484, debug=True)
+    app.run(port=5794, debug=True)
---
+++ const_data.py
@@ -0,0 +1,19 @@
+lat_default = 47.560053
+long_default = -122.213896
+renovated_default = 'not renovated'
+built_default = 'middle'
+basement_default = 'has basement'
+sqft_living_default = 'medium'
+sqft_lot_default = 'medium'
+sqft_above_default = 'medium'
+lat_cat_default = 'north'
+long_cat_default = 'east'
+bedrooms_default = 4
+bathrooms_default = 2
+floors_default = 1
+waterfront_default = 0
+view_default = 0
+condition_default = 3
+grade_default = 7
+
+
